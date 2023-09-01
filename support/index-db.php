73bf063cce29e509b7fa1f2de043f92eb58d43a1
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-09-01 16:22:44 +0700

    updta

--- support/index-db.php
+++ support/index-db.php
@@ -28,35 +28,74 @@ if (
   // ปิดการเชื่อมต่อกับฐานข้อมูล
   $mysqli->close();
 
-  // เรียกใช้งาน SweetAlert ตามผลลัพธ์การอัปเดต
-  echo '
+  // ตรวจสอบผลลัพธ์การอัปเดตและแจ้งเตือน Line Notify
+  if ($result) {
+    $sToken = ["MJ8OSZyno8b7v2LqVexPMP4KuWhs5z93fS84Zhltc89"]; // เปลี่ยนเป็น Line Notify Token ของคุณ
+    $sMessage = "Booking\r\n";
+    $sMessage .= "\n";
+    $sMessage .= "Booking Id: " . $booking_id . "\n";
+    $sMessage .= "Date: " . $date . "\n";
+    $sMessage .= "Time: " . $timeslot . "\n";
+    $sMessage .= "Service Type: " . $title . "\n";
+    $sMessage .= "Meeting Option: \n";
+    $sMessage .= $meeting . "\n";
+    $sMessage .= "\n";
+    $sMessage .= "Booked by: " . $name . "\n";
+    $sMessage .= "E-mail: " . $email . "\n";
+    $sMessage .= "Tel: " . $tel . "\n";
+
+    // สร้างฟังก์ชัน notify_message ภายในส่วน PHP ของคุณ
+    function notify_message($sMessage, $Token)
+    {
+      $chOne = curl_init();
+      curl_setopt($chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify");
+      curl_setopt($chOne, CURLOPT_SSL_VERIFYHOST, 0);
+      curl_setopt($chOne, CURLOPT_SSL_VERIFYPEER, 0);
+      curl_setopt($chOne, CURLOPT_POST, 1);
+      curl_setopt($chOne, CURLOPT_POSTFIELDS, "message=" . $sMessage);
+      $headers = array('Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer ' . $Token . '',);
+      curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($chOne, CURLOPT_RETURNTRANSFER, 1);
+      $result = curl_exec($chOne);
+      if (curl_error($chOne)) {
+        echo 'error:' . curl_error($chOne);
+      }
+      curl_close($chOne);
+    }
+
+    // เรียกใช้งานฟังก์ชัน notify_message สำหรับทุก Token
+    foreach ($sToken as $Token) {
+      notify_message($sMessage, $Token);
+    }
+
+    // แสดงแจ้งเตือน SweetAlert หลังจากการอัปเดตเสร็จสิ้น
+    echo '
         <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">';
-
-  if ($result) {
     echo '<script>
-            swal({
-              title: "Updata Booking Success",
-              text: "success",
-              type: "success",
-              timer: 1500,
-              showConfirmButton: false
-            }, function(){
-              window.location = "calendar.php";
-            });
-          </script>';
+        swal({
+          title: "Update Booking Success",
+          text: "Success",
+          type: "success",
+          timer: 1500,
+          showConfirmButton: false
+        }, function(){
+          window.location = "calendar.php";
+        });
+      </script>';
   } else {
+    // แสดงแจ้งเตือน SweetAlert หากการอัปเดตล้มเหลว
     echo '<script>
-            swal({
-              title: "Updata Booking Fail",
-              text: "fail",
-              type: "fail",
-              timer: 1500,
-              showConfirmButton: false
-            }, function(){
-              window.location.href = "calendar.php";
-            });
-          </script>';
+        swal({
+          title: "Update Booking Fail",
+          text: "Fail",
+          type: "error",
+          timer: 1500,
+          showConfirmButton: false
+        }, function(){
+          window.location.href = "calendar.php";
+        });
+      </script>';
   }
 }
